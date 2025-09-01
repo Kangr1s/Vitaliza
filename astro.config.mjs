673e0b804b89d60bf9a7c7cf1e3627161d64c39b
SapoPerroDev <bgutierrez@unimayor.edu.co>
2025-09-01 00:09:13 -0500

    fix(config): change static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({  
     site: "https://Kangr1s.github.io",
-    base: "/Vitaliza/",
-    output: 'server',
+    base: "/Vitaliza",
+    output: 'static',
     adapter: node({
         mode: 'standalone', // o 'server', según lo necesites
     }),

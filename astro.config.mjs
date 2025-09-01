66513d435db7e028cc2e2aa304a4a2f859bda111
SapoPerroDev <bgutierrez@unimayor.edu.co>
2025-08-31 23:32:58 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({  
     site: "https://Kangr1s.github.io",
-    base: "Vitaliza"
+    base: "Vitaliza",
+    output: 'server',
+    adapter: node({
+        mode: 'standalone', // o 'server', según lo necesites
+    }),
 });

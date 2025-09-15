bec1c6a2d3f8be8950d84b74c7b0cd6066d85b2c
SapoPerroDev <bgutierrez@unimayor.edu.co>
2025-09-15 00:18:33 -0500

    feat(img): change img carrusel and team

--- src/data/team.js
+++ src/data/team.js
@@ -1,20 +1,20 @@
 export const equipo = [
   {
-    img: "/img/team-1.jpg",
+    img: "/img/team_1.png",
     nombre: "Steffany Rios",
     profesion: "Fisioterapeuta especializado en masajes terapéuticos",
     instagram: "http://instagram.com/",
     linkedin: "http://linkedin.com/in/"
   },
   {
-    img: "/img/team-2.jpg",
+    img: "/img/team_2.png",
     nombre: "Alejandro Artunduaga",
     profesion: "Especialista en rehabilitación y terapia física",
     facebook: "http://facebook.com/",
     instagram: "http://instagram.com/"
   },
   {
-    img: "/img/team-3.jpg",
+    img: "/img/team_3.png",
     nombre: "Luciana Luces",
     profesion: "Doctor en terapia física y rehabilitación",
     facebook: "http://facebook.com/",
@@ -22,7 +22,7 @@ export const equipo = [
     instagram: "http://instagram.com/",
   },
   {
-    img: "/img/team-4.jpg",
+    img: "/img/team_4.png",
     nombre: "Hernesto Junior",
     profesion: "Doctor en terapia física y rehabilitación",
     facebook: "http://facebook.com/",
